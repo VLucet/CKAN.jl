922ab04784e762a4dbf422b459657743196c424b
Valentin <valentin.lucet@gmail.com>
2022-01-30 22:20:15 -0500

    add package serach

--- src/actions.jl
+++ src/actions.jl
@@ -1,4 +1,5 @@
-function package_show(connection::CKANConnection, id::String; querryres::Bool=true)
+function package_show(connection::CKANConnection, 
+                      id::String; querryres::Bool=true)
     url = connection.url
     querry = package_show(url, id)
     result = querry["result"]
@@ -32,7 +33,7 @@ function resource_show(url::String, id::String)
 end
 
 function package_list(connection::CKANConnection; 
-                      querryres::Bool=true, limit::Int64=100)
+                      querryres::Bool=true, limit::Int64=20)
     url = connection.url
     querry = package_list(url)
     package_ids = querry["result"]
@@ -48,3 +49,33 @@ end
 function package_list(url::String)
     return querry_api(url, "package_list", ("" => ""))
 end
+
+function package_search(connection::CKANConnection, parameters::Pair...; 
+                        querrypkg::Bool=true, querryres::Bool=true)
+    url = connection.url
+    querry = package_search(url, parameters...)
+    
+    result = querry["result"]
+    count = result["count"]
+    
+    dicts = result["results"]
+    ids = [x["id"] for x in dicts]
+
+    @info "Search returned $count results"
+    if querrypkg
+        return package_show.([connection], ids, querryres=querryres)
+    else
+        return result
+    end
+end
+
+function package_search(url::String, parameters::Pair...)
+    return querry_api(url, "package_search", parameters...)
+end
+
+# function resource_search(connection::CKANConnection, parameters::Pair)
+# end
+
+# function resource_search(url::String, parameters::Pair)
+#     return querry_api(url, "resource_search", parameters)
+# end
