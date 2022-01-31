d46a5ca14778638a388ad736d4c0fccce15543e9
Valentin <valentin.lucet@gmail.com>
2022-01-30 22:20:27 -0500

    splat querry

--- src/CKAN.jl
+++ src/CKAN.jl
@@ -6,6 +6,9 @@ using JSON
 export package_list, package_show, resource_list, resource_show
 export CKANConnection, CKANDataset, CKANResource
 
+const web_root = "https://open.canada.ca/data/en/"
+const api_root = web_root * "api/3/"
+
 include(joinpath(".", "types.jl"))
 include(joinpath(".", "show.jl"))
 include(joinpath(".", "url.jl"))
