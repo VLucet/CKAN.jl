4de313152255eaadf129f7670b0078f9f30f8167
Documenter.jl <documenter@juliadocs.github.io>
2022-02-02 05:29:08 +0000

    build based on aa1e021

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#CKANQuerry.jl","page":"CKANQuerry.jl","title":"CKANQuerry.jl","text":"","category":"section"},{"location":"","page":"CKANQuerry.jl","title":"CKANQuerry.jl","text":"Documentation for CKANQuerry.jl","category":"page"}]
+[{"location":"#CKANQuerry.jl","page":"CKANQuerry.jl","title":"CKANQuerry.jl","text":"","category":"section"},{"location":"","page":"CKANQuerry.jl","title":"CKANQuerry.jl","text":"Documentation for CKANQuerry.jl","category":"page"},{"location":"","page":"CKANQuerry.jl","title":"CKANQuerry.jl","text":"Modules: [CKANQuerry]","category":"page"}]
 }
