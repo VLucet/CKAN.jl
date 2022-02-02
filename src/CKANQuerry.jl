f9c64c8ff8a1be3325f22a99090b102025da53ba
Valentin <valentin.lucet@gmail.com>
2022-02-02 00:35:40 -0500

    fix issues

--- src/CKANQuerry.jl
+++ src/CKANQuerry.jl
@@ -1,5 +1,18 @@
 module CKANQuerry
 
-# Write your package code here.
+using HTTP
+using JSON
+
+export package_list, package_show, resource_list, resource_show
+export CKANConnection, CKANDataset, CKANResource
+
+const web_root = "https://open.canada.ca/data/en/"
+const api_root = web_root * "api/3/"
+
+include(joinpath(".", "types.jl"))
+include(joinpath(".", "show.jl"))
+include(joinpath(".", "url.jl"))
+include(joinpath(".", "query.jl"))
+include(joinpath(".", "actions.jl"))
 
 end
