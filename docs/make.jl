ac39c61a8de301b001e26a199dcdef5c1b7d8770
Valentin <valentin.lucet@gmail.com>
2022-02-02 00:23:01 -0500

    doc should now work

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [CKANQuerry]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/VLucet/CKANQuerry.jl"
+)
